226b5473c0e595e850fc5b8393068f9a64adda38
CHC <chc@openspy.net>
2025-12-07 12:07:43 -0500

    implemented server expire

--- src/Handlers/ServerExpirationHandler.go
+++ src/Handlers/ServerExpirationHandler.go
@@ -13,6 +13,8 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
+const SERVER_EXPIRE_SCAN_BATCH_COUNT int64 = 500
+
 type ServerExpirationHandler struct {
 	context      context.Context
 	serverMgr    Server.IServerManager
@@ -21,6 +23,10 @@ type ServerExpirationHandler struct {
 
 	amqpChannel  *amqp.Channel
 	resyncTicker *time.Ticker
+}
+
+func (m *ServerExpirationHandler) selectServerRedisDB(context context.Context, redisClient *redis.Client) {
+	redisClient.Conn().Select(context, 0)
 }
 
 func (h *ServerExpirationHandler) getResyncInterval() time.Duration {
@@ -32,14 +38,14 @@ func (h *ServerExpirationHandler) getResyncInterval() time.Duration {
 	}
 	return time.Duration(val) * time.Second
 }
-func (h *ServerExpirationHandler) getMinTTL() int {
+func (h *ServerExpirationHandler) getMinTTL() time.Duration {
 	var interval_str = os.Getenv("SERVER_EXPIRE_MIN_TTL_SECS")
 	val, err := strconv.Atoi(interval_str)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "server expire getMinTTL env parse error: %s\n", err.Error())
-		val = 60
+		val = 180
 	}
-	return val
+	return time.Duration(val) * time.Second
 }
 
 func (h *ServerExpirationHandler) HandleNewServer(serverKey string) {
@@ -105,4 +111,57 @@ func (h *ServerExpirationHandler) doExpirationScan() {
 	//do scan and pipelined TTL
 	//if TTL < min ttl, append to delete list
 	//then set deletes (pipelined) and send event
+
+	h.selectServerRedisDB(h.context, h.redisClient)
+
+	var cursor int = 0
+	for {
+		keys, nextCursor, err := h.redisClient.Scan(h.context, uint64(cursor), "*:", SERVER_EXPIRE_SCAN_BATCH_COUNT).Result()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "doExpirationScan server scan error: %s\n", err.Error())
+			break
+		}
+		cursor = int(nextCursor)
+
+		ttlCheckPipeline := h.redisClient.Pipeline()
+		var ttlCmds []*redis.DurationCmd
+		var ttlGetDel []*redis.StringCmd
+		for _, key := range keys {
+			ttlCmds = append(ttlCmds, ttlCheckPipeline.TTL(h.context, key))
+			ttlGetDel = append(ttlGetDel, ttlCheckPipeline.HGet(h.context, key, "deleted"))
+		}
+
+		_, ttlCheckErr := ttlCheckPipeline.Exec(h.context)
+		if ttlCheckErr != nil {
+			fmt.Fprintf(os.Stderr, "doExpirationScan ttl check error: %s\n", ttlCheckErr.Error())
+			break
+		}
+
+		deletePipeline := h.redisClient.Pipeline()
+		var deletedKeys []string
+		for idx, key := range keys {
+			if ttlCmds[idx].Val() > h.getMinTTL() {
+				continue
+			}
+			var deleted = ttlGetDel[idx].Val()
+			deletedVal, _ := strconv.Atoi(deleted)
+			if deletedVal != 0 {
+				continue
+			}
+			deletePipeline.HSet(h.context, key, "deleted", "1")
+			deletedKeys = append(deletedKeys, key)
+		}
+		_, deleteErr := deletePipeline.Exec(h.context)
+		if deleteErr != nil {
+			fmt.Fprintf(os.Stderr, "doExpirationScan delete error: %s\n", deleteErr.Error())
+			break
+		}
+		for _, key := range deletedKeys {
+			h.sendServerExpireMessage(key)
+		}
+
+		if cursor == 0 {
+			break
+		}
+	}
 }

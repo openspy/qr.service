c43ee0fb03582be343215fea581531a02ed51823
CHC <chc@openspy.net>
2025-12-14 18:06:38 -0500

    removed useless comment

--- src/Server/IGameManager.go
+++ src/Server/IGameManager.go
@@ -6,7 +6,6 @@ import (
 	"github.com/redis/go-redis/v9"
 )
 
-// group redis db = 1
 type IGameManager interface {
 	GetGameKey(context context.Context, redisClient *redis.Client, gameid int) string
 	GetBackendFlags(context context.Context, redisClient *redis.Client, gameKey string) int
